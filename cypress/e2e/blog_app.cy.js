ba3d88a04485b6727a563c2ec6195582bd1ffcf8
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-24 12:37:24 +0530

    bloglist end to end testing, step2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Naseef',
+      username: 'nf123',
+      password: 'nfpw123',
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,25 @@ describe('Blog app', function () {
     cy.contains('login')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('nf123')
+      cy.get('#password').type('nfpw123')
+      cy.get('#login-button').click()
+      cy.contains('Naseef logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('nf123')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Naseef logged in')
+    })
   })
 })
--- src/App.js
+++ src/App.js
@@ -95,6 +95,7 @@ const App = () => {
       <div>
         username
         <input
+          id="username"
           type="text"
           value={username}
           name="Username"
@@ -104,13 +105,16 @@ const App = () => {
       <div>
         password
         <input
+          id="password"
           type="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">
+        login
+      </button>
     </form>
   )
   const blogList = () => (
